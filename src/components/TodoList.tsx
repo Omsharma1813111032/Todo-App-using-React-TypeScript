8665971c9c86a2ee11297469f8a2d7eb1a9a9a2d
Om Sharma <omsharma1813111032@gmail.com>
2024-02-14 13:31:22 +0530

    Project Done

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,23 +1,38 @@
 import React from 'react'
 import "./Style.css"
 import { Todo } from '../model'
+import { MdDelete,MdOutlineDone } from "react-icons/md";
 
 interface Props{
     todos:Todo,
     setTodos:React.Dispatch<React.SetStateAction<Todo>>
 }
 
+
+const handleDelete = (e: React.MouseEvent<SVGElement, MouseEvent>,id:number, setTodos:React.Dispatch<React.SetStateAction<Todo>>,todos:Todo) =>{
+    e.preventDefault()
+    setTodos(todos.filter((todo) => todo.id !== id))
+}
+
+
+
 const TodoList = ({todos,setTodos}:Props) => {
   return (
     <div className='todos'>
-        {todos.map((dt)=>(       
-            <div>     
-                <li>
-                    {dt.todo}
-                    {dt.id}
-                </li>
-            </div>
-       ))}
+        {
+            todos.length > 0 ? todos.map((dt:Todo)=>(       
+                <div className='li_wrapper'>     
+                    <li key={dt.id} >
+                        {dt.todo}
+                        <div className='todos_action'>
+                            <MdDelete onClick={(e)=>{handleDelete(e,dt.id,setTodos,todos)}} className='delete_icon' />
+                        </div>
+                    </li>
+                </div>
+           ))
+           : "Add new task!!"
+        }
+        
     </div>
   )
 }
