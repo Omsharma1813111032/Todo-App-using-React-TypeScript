9e4816fedbace49fd1055b45954637aee331bc57
Om Sharma <omsharma1813111032@gmail.com>
2024-02-14 07:36:08 +0530

    Require Model Added and Maintaining State

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Style.css"
+import { Todo } from '../model';
 
-const InputField:React.FC = () => {
+interface Props{
+  todo:string;
+  setTodo:React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField:({ todo, setTodo}: Props) => JSX.Element= ({todo,setTodo}:Props) => {
+
+  const [todos,setTodos] = useState<Todo>([])
+
+  const handleAdd = (e: React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+    if(todo){
+      setTodos([...todos,{id:Date.now(),todo,isDone:false}])
+      setTodo("")
+    }
+  }
+  
+  console.log(todos)
+
   return (
-    <form className='input'>
-        <input type='input' placeholder='Enter a Task' className='input_box' />
+    <form className='input' onSubmit={(e)=>{handleAdd(e)}} >
+        <input type='input' placeholder='Enter a Task' value={todo} onChange={(e)=>{setTodo(e.target.value)}} className='input_box'/>
         <button className='input_submit' type='submit' >Go</button>
     </form>
   )
